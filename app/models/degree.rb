b594fc7ebebf997833dd8e2b00fa5d3564622a10
Christopher Bruen <bruen.chris@gmail.com>
2016-09-09 00:06:41 +0000

    add figaro gem

--- app/models/degree.rb
+++ app/models/degree.rb
@@ -5,7 +5,8 @@ class Degree < ActiveRecord::Base
 
 	validates :name, presence: true
 	validates :school_id, presence: true
-
+	validates :gpa, numericality: true
+	
 	def school_attributes=(att)
 		if School.find_or_create_by(name: att[:name], city: att[:city]).valid?
 			self.school = School.find_or_create_by(name: att[:name], city: att[:city])
