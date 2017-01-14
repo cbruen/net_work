e10dce185ce17e9eb86e78a53522dc7f2043e50c
Chris Bruen <bruen.chris@gmail.com>
2017-01-14 16:40:47 -0500

    move javascript from degrees index to javascript file. use for user show page, as well

---
+++ app/assets/javascripts/javascript.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+//  test();
+
+});
+
+function test() {
+  console.log("AHH");
+}
+
+
+
+function add() {
+  var id = $('#degrees').data("id");
+  var url = "/users/" + id + "/degrees";
+  var method = "GET";
+  $.ajax({
+    url: url,
+    method: method,
+    dataType: "json"
+  }).done(function(data) {
+    data["degrees"].forEach(function(x) {
+      var list = "<h3>" + x["school"]["name"] + " (" + x["school"]["city"] + ")</h3>";
+      list += "<p>" + x["name"] + "</p>";
+      list += "<p>GPA: " + x["gpa"] + "</p></br>";
+      $('#degrees').append(list);
+    });
+  });
+}
